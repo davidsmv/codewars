4375e40e0544bdb92e9f7d71dbfd54a95fab3d0f
David Martínez <david-martinezv@outlook.com>
2022-03-30 19:19:11 -0500

    agrego ejercicio de odd numbers prueba 2

--- oddnumber.py
+++ oddnumber.py
@@ -8,7 +8,7 @@
 
 import collections
 def find_it(seq):
-    #hace un diccionario contando cuántos hay de cada dato
+    #hace un diccionario contando cuantos hay de cada dato
     datos = collections.Counter(seq)
     for i in datos:
         if (datos[i] % 2) > 0:       
@@ -16,21 +16,21 @@ def find_it(seq):
 
 #sampletest
 
-import codewars_test as test
-from solution import find_it
+# import codewars_test as test
+# from solution import find_it
 
-@test.describe("Sample tests")
-def sample_tests():
+# @test.describe("Sample tests")
+# def sample_tests():
     
-    @test.it("find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) should return 5 (because it appears 3 times)")
-    def _():
-        test.assert_equals(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
+#     @test.it("find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) should return 5 (because it appears 3 times)")
+#     def _():
+#         test.assert_equals(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
         
-    @test.it("find_it([1,1,2,-2,5,2,4,4,-1,-2,5]) should return -1 (because it appears 1 time)")
-    def _():
-        test.assert_equals(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]), -1);
+#     @test.it("find_it([1,1,2,-2,5,2,4,4,-1,-2,5]) should return -1 (because it appears 1 time)")
+#     def _():
+#         test.assert_equals(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]), -1);
         
-    @test.it("find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]) should return 5 (because it appears 3 times)")
-    def _():
-        test.assert_equals(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]), 5);
+#     @test.it("find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]) should return 5 (because it appears 3 times)")
+#     def _():
+#         test.assert_equals(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]), 5);
         
