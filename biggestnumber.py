6c4ff6f8db2880931990542aa2d3f0c01fd53ded
David Martínez <david-martinezv@outlook.com>
2022-04-18 19:33:55 -0500

    Ejerccios de trolls y encontrar el número más grande

---
+++ biggestnumber.py
@@ -0,0 +1,20 @@
+# Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+# Examples:
+# Input: 42145 Output: 54421
+
+# Input: 145263 Output: 654321
+
+# Input: 123456789 Output: 987654321
+
+
+
+def descending_order(num):
+    lista = []
+    #for para dejar todos los números independientes en una lista
+    for i in str(num):
+        lista.append(int(i))
+    #para organizar todos los datos de mayor a menor
+    lista.sort(reverse=True)
+    resultado = "".join(map(str,lista))
+    return int(resultado)
