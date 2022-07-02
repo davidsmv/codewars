e235a6a3bfec81c5fb13b5f3ac1cc006bf497893
David Martínez <david-martinezv@outlook.com>
2022-07-02 17:28:29 -0500

    -

---
+++ sql_HackeRank.sql
@@ -0,0 +1,7 @@
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+-- https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+
+select round(LONG_W,4)
+from station
+where LAT_N = (select max(LAT_N) from station where  LAT_N < 137.2345)
